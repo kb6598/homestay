38d03029953a468d1f43cde04359a4aa45c28790
나평근 노트북 <skvudrms@daum.net>
2019-10-31 02:00:54 +0900

    Revert "add in laptop"
    This reverts commit 75d9e36cf0f4b9db5268560c0bb0f0ec78f80ea9.

--- test/src/main/java/com/spring/test/HomeController.java
+++ test/src/main/java/com/spring/test/HomeController.java
@@ -56,9 +56,6 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("serverTime2", formattedDate );
-		model.addAttribute("serverTime3", formattedDate );
-		model.addAttribute("serverTime4", formattedDate );
 		
 		return "home";
 	}
