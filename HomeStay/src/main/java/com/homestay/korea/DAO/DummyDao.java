f70c40a13df7d07b2637fbc72483f0d6351ad8c5
나평근 <skvudrms54@gmail.com>
2019-11-01 19:39:44 +0900

    수정

--- HomeStay/src/main/java/com/homestay/korea/DAO/DummyDao.java
+++ HomeStay/src/main/java/com/homestay/korea/DAO/DummyDao.java
@@ -1,12 +1,7 @@
 package com.homestay.korea.DAO;
 
-<<<<<<< HEAD
-public class DummyDao {
-	
-=======
 
 public class DummyDao {
->>>>>>> 0f53da553badb573aedccbf0d15e300a74aa2d93
 
 	
 }
--- HomeStay/src/main/java/com/homestay/korea/service/DummyService.java
+++ HomeStay/src/main/java/com/homestay/korea/service/DummyService.java
@@ -2,10 +2,6 @@ package com.homestay.korea.service;
 
 
 public class DummyService {
-<<<<<<< HEAD
-	
-=======
->>>>>>> 0f53da553badb573aedccbf0d15e300a74aa2d93
 
 	
 }
