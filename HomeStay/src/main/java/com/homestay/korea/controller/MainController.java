49141408f4dbb6170a5dae024034126eee7e62af
나평근 <skvudrms54@gmail.com>
2019-11-01 21:53:12 +0900

    MainController 추가

--- HomeStay/src/main/java/com/homestay/korea/controller/MainController.java
+++ HomeStay/src/main/java/com/homestay/korea/controller/MainController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.homestay.korea.HomeController;
 
 @Controller
-
+@RequestMapping("/mainpage")
 public class MainController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -23,10 +23,35 @@ public class MainController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/topview", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String topview(Locale locale, Model model) {
 
 		
 		return "homestay/mainPage/topView";
+	}
+	@RequestMapping(value = "/menu", method = RequestMethod.GET)
+	public String menu(Locale locale, Model model) {
+
+		
+		return "homestay/mainPage/menu";
+	}
+	@RequestMapping(value = "/mainview", method = RequestMethod.GET)
+	public String mainview(Locale locale, Model model) {
+
+		
+		return "homestay/mainPage/mainView";
+	}
+	@RequestMapping(value = "/subview", method = RequestMethod.GET)
+	public String subview(Locale locale, Model model) {
+
+		
+		return "homestay/mainPage/subView";
+	}
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(Locale locale, Model model) {
+
+		
+		return "homestay/mainPage/index";
 	}
 	
 	
