efc181dd4e845ff38015c14124d8bd94343029b6
나평근 <skvudrms54@gmail.com>
2019-11-03 21:33:49 +0900

    db 수정 사항 DTO에 반영  tour_image:contentId2 -> contentId

--- HomeStay/src/main/java/com/homestay/korea/DTO/TourImageDTO.java
+++ HomeStay/src/main/java/com/homestay/korea/DTO/TourImageDTO.java
@@ -3,7 +3,7 @@ package com.homestay.korea.DTO;
 public class TourImageDTO {
 	int imageNo;
 	String imageUrl;
-	String contentId2;
+	String contentId;
 	public int getImageNo() {
 		return imageNo;
 	}
@@ -17,10 +17,10 @@ public class TourImageDTO {
 		this.imageUrl = imageUrl;
 	}
 	public String getContentId2() {
-		return contentId2;
+		return contentId;
 	}
-	public void setContentId2(String contentId2) {
-		this.contentId2 = contentId2;
+	public void setContentId2(String contentId) {
+		this.contentId = contentId;
 	}
 	@Override
 	public String toString() {
