fb0b5c2a9bf670b7803fa5c761e24bbdb4cebe4e
나평근 <skvudrms54@gmail.com>
2019-12-03 06:52:15 +0900

    Add ValidationQeury

--- HomeStay/src/main/java/com/homestay/korea/config/RootConfig.java
+++ HomeStay/src/main/java/com/homestay/korea/config/RootConfig.java
@@ -26,14 +26,22 @@ public class RootConfig {
 	public DataSource dataSource() {
 		DataSource ds=new DataSource();
 		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		ds.setUrl("jdbc:mysql://itbuddy.iptime.org:8233/capstone?autoReconnect=true"); 
+		ds.setUrl("jdbc:mysql://localhost:8233/capstone"); 
 		ds.setUsername("capstone"); 
 		ds.setPassword("capstone1234"); 
-//		ds.setInitialSize(2);
-//		ds.setMaxActive(10);
-//		ds.setTestWhileIdle(true);
-//		ds.setMinEvictableIdleTimeMillis(60000*3);
-//		ds.setTimeBetweenEvictionRunsMillis(10*1000);
+		ds.setInitialSize(2);		
+		ds.setMaxActive(10);
+		
+		
+		
+		//특정 시간마다 Validation query를 실행 
+		//mysql에서는 기본 8시간동안 DB 요청이 없으면 커넥션을 끊음
+		//따라서 일정 시간마다 연결 확인 쿼리를 보내주어야함
+		ds.setTestWhileIdle(true);
+		ds.setValidationQuery("select 1");
+		ds.setMinEvictableIdleTimeMillis(60000*3);
+		ds.setTimeBetweenEvictionRunsMillis(10*1000);
+		ds.setValidationQueryTimeout(2880);
 		return ds;
 	}
 
